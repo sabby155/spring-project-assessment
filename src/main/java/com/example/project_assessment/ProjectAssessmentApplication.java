9d9f6ada940b52716fc65fdedb45daa18606b4eb
sabby155 <nebula155@gmail.com>
2022-08-11 10:30:14 -0400

    init repo

---
+++ src/main/java/com/example/project_assessment/ProjectAssessmentApplication.java
@@ -0,0 +1,13 @@
+package com.example.project_assessment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectAssessmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectAssessmentApplication.class, args);
+	}
+
+}
